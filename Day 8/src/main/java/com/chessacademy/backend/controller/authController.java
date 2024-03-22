3f550de444291f394e48c327c1aa03085f75e688
PRANESHWAR G <144886491+praneshwar05@users.noreply.github.com>
2024-03-22 17:53:41 +0530

    Add files via upload

---
+++ Day 8/src/main/java/com/chessacademy/backend/controller/authController.java
@@ -0,0 +1,41 @@
+package com.chessacademy.backend.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tech.chessacademy.backend.dto.AuthRequest;
+import com.tech.chessacademy.backend.service.JwtService;
+
+@RestController
+@RequestMapping("/api/auth")
+public class authController {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @GetMapping("/home")
+    public String Home() {
+        return "Initial Render Page.!";
+    }
+
+    @PostMapping("/authenticate")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword()));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getEmail());
+        } else {
+            throw new UsernameNotFoundException("Invalid user request!");
+        }
+    }
+}
